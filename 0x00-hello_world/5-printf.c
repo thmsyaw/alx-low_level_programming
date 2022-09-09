f1ed9b27fb676e13dff80b9662d082c97cb81de0
Thomas Bawa <thmsyaw@gmail.com>
2022-09-09 10:45:07 -0700

    6th task commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - prints the string in the printf function
+*
+* Return:0 if exited properly, otherwise non-zero
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
