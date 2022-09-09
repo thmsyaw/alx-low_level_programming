347d2386a2b2e20261c9a27eacc6fdde3befbaa5
Thomas Bawa <thmsyaw@gmail.com>
2022-09-09 09:11:07 -0700

    5th task commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include stdio.h
+
+/**
+ * main - prints the phrase " Programming is like building a multi lingual puzzle"
+ * Return: 0 if exited properly and a non zero if otherwise
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
