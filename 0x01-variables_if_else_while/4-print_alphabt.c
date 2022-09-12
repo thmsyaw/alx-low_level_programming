68317e45f3a680e158da7931b8697d714a0680a6
Thomas Bawa <thmsyaw@gmail.com>
2022-09-12 06:35:23 -0700

    4th task commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Description: prints all lower case
+*letters except e or q
+*Return: 0
+*/
+int main(void)
+{
+	char ch = 'a'
+	
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
