b44f5dadc43fba5d894f5e6c15e550ed35a99d4e
Thomas Bawa <thmsyaw@gmail.com>
2022-09-12 09:17:09 -0700

    9th task commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Description: prints all possible different combinations
+*of two digits
+*Return: 0
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 57 || m !=57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
